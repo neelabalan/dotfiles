72718e5e4cf6ca3330da83bd473627ee75981e2c
neelabalan <neelabalan.n@outlook.com>
2025-07-21 06:04:15 +0530

    neovim setup: indent, autopair and misc settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,9 +14,67 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Plugin configuration
 require("lazy").setup({
-  "nvim-lua/plenary.nvim",           -- Common utilities
+  -- Common utilities
+  "nvim-lua/plenary.nvim",           
   {
-    "nvim-telescope/telescope.nvim",  -- Modern fuzzy finder
+    -- Auto pairs for brackets, quotes, etc.
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup({
+        check_ts = true,
+        ts_config = {
+          lua = {'string', 'source'},
+          javascript = {'string', 'template_string'},
+          java = false,
+        },
+        disable_filetype = { "TelescopePrompt", "spectre_panel" },
+        fast_wrap = {
+          map = '<M-e>',
+          chars = { '{', '[', '(', '"', "'" },
+          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+          offset = 0,
+          end_key = '$',
+          keys = 'qwertyuiopzxcvbnmasdfghjkl',
+          check_comma = true,
+          highlight = 'PmenuSel',
+          highlight_grey = 'LineNr'
+        },
+      })
+    end
+  },
+  {
+    -- Better indentation visualization
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require("ibl").setup({
+        indent = {
+          char = "│",
+          tab_char = "│",
+        },
+        scope = { enabled = false },
+        exclude = {
+          filetypes = {
+            "help",
+            "alpha",
+            "dashboard",
+            "neo-tree",
+            "Trouble",
+            "trouble",
+            "lazy",
+            "mason",
+            "notify",
+            "toggleterm",
+            "lazyterm",
+          },
+        },
+      })
+    end
+  },           
+  {
+    -- Modern fuzzy finder
+    "nvim-telescope/telescope.nvim",  
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files<cr>')
@@ -24,24 +82,80 @@ require("lazy").setup({
     end
   },
   {
-    "nvim-tree/nvim-tree.lua",        -- Modern file explorer
+    -- Modern file explorer
+    "nvim-tree/nvim-tree.lua",        
     config = function()
       require("nvim-tree").setup()
       vim.keymap.set('n', '<C-n>', '<cmd>NvimTreeToggle<cr>')
     end
   },
   {
-    "nvim-lualine/lualine.nvim",      -- Modern statusline
+    -- Modern statusline
+    "nvim-lualine/lualine.nvim",      
     config = function()
-      require('lualine').setup()
+      -- Custom theme based on your color scheme
+      local custom_theme = {
+        normal = {
+          a = { fg = '#000000', bg = '#81a2be' },  -- Blue background
+          b = { fg = '#ffffff', bg = '#444444' },
+          c = { fg = '#a8a8a8', bg = '#242424' }
+        },
+        insert = {
+          a = { fg = '#000000', bg = '#9ec400' },  -- Green background
+          b = { fg = '#ffffff', bg = '#444444' }
+        },
+        visual = {
+          a = { fg = '#000000', bg = '#f0c674' },  -- Yellow background
+          b = { fg = '#ffffff', bg = '#444444' }
+        },
+        replace = {
+          a = { fg = '#ffffff', bg = '#c70000' },  -- Red background
+          b = { fg = '#ffffff', bg = '#444444' }
+        },
+        command = {
+          a = { fg = '#000000', bg = '#b77ee0' },  -- Purple background
+          b = { fg = '#ffffff', bg = '#444444' }
+        },
+        inactive = {
+          a = { fg = '#70c0ba', bg = '#444444' },  -- Cyan for inactive
+          b = { fg = '#666666', bg = '#353535' },
+          c = { fg = '#969696', bg = '#242424' }
+        }
+      }
+      
+      require('lualine').setup({
+        options = {
+          theme = custom_theme,
+          component_separators = { left = '', right = ''},
+          section_separators = { left = '', right = ''},
+          icons_enabled = false,
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
+        }
+      })
     end
   },
   {
-    "Mofiqul/vscode.nvim",            -- VSCode theme
+    -- VSCode theme
+    "Mofiqul/vscode.nvim",            
     config = function()
       require('vscode').setup({
         transparent = false,
-        italic_comments = true,
+        italic_comments = false,
         disable_nvimtree_bg = true,
       })
       vim.cmd("colorscheme vscode")
@@ -51,9 +165,23 @@ require("lazy").setup({
 
 -- Basic settings
 vim.opt.number = true
+vim.opt.numberwidth = 2
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.cindent = false
+vim.opt.shiftround = false
+vim.opt.preserveindent = false
+vim.opt.copyindent = false
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
 vim.opt.mouse = "a"
+vim.opt.guicursor = "n-v-c-i:block"
+vim.opt.showmode = false
+
+-- Additional indentation configuration
+vim.opt.smarttab = true
+vim.opt.breakindent = true
--- README.md
+++ README.md
@@ -8,6 +8,6 @@ alias dotsync='rsync -avzPR $(cat .dotlist) $HOME/.dotfiles/'
 ```
 
 
-`python3.11 envcraft.py --arch=aarch64 --mode=docker`
+`python3.11 devenv/envcraft.py --arch=aarch64 --mode=docker`
 
-`python3.11 envcraft.py --arch=x86_64 --mode=docker`
+`python3.11 devenv/envcraft.py --arch=x86_64 --mode=docker`
